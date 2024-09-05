948911c27f89a68803e88b62e350d80836532484
Krasidimitrov-sudo <krasi123dim@gmail.com>
2024-09-05 11:11:10 +0300

    backend-2-filter

--- backend/listDocument.js
+++ backend/listDocument.js
@@ -5,22 +5,28 @@ let Item_list = [
     { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Chair" , sizes: "s,m,l", colors : "black, ect."}, 
     { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Chair" , sizes: "s,m,l", colors : "black, ect."}, 
     { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Chair" , sizes: "s,m,l", colors : "black, ect."}, 
+    { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Sofa" , sizes: "s,m,l", colors : "black, ect."}, 
+    { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Sofa" , sizes: "s,m,l", colors : "black, ect."}, 
+    { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Sofa" , sizes: "s,m,l", colors : "black, ect."}, 
+    { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Sofa" , sizes: "s,m,l", colors : "black, ect."}, 
+    { name: "", short_description: "" , description: "" , price: 0 ,discount: 0 , quality: 0 , category: "Sofa" , sizes: "s,m,l", colors : "black, ect."}, 
+
 ];
 
 function filterProducts(Item_list, criteria) {
     return Item_list.filter(product => {
-        let matches = true;
         for (let key in criteria) {
-            if (product[key] !== criteria[key]) {
-                matches = false;
-                break;
+            if (product[key] == criteria[key]) {
+                return true;
             }
         }
-        return matches;
+        return false;
     });
 }
 
-let filteredChair = filterProducts(Item_list, { category: "Chair" });
-console.log(filteredChair);
+let filteredChair = [filterProducts(Item_list, { category: "Chair" })];
+console.log(JSON.stringify(filteredChair, null, 2));
 
+let filteredSofa =[ filterProducts(Item_list, { category: "Sofa" })];
+console.log(JSON.stringify(filteredSofa ,null , 2 ));
 
