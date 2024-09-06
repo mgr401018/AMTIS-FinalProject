3cca6e536b26b235d0ffd2fcef4ee7d8dec2a05f
DandelionChan <80709919+DandelionChan@users.noreply.github.com>
2024-09-07 00:54:19 +0300

    The beginning of the second page

--- frontend/backendload.js
+++ frontend/backendload.js
@@ -7,3 +7,15 @@ window.addEventListener("load", () => {
       console.log(response);
     });
 });
+
+window.addEventListener("load", () => {
+  fetch("http://localhost:3000/carrousel")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(response) {
+    for(let i = 0; i<=response.length-1; i++) {
+      response[i].title;
+    }
+  });
+});
